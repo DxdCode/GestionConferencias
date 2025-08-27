680f734fb18eb44b40483f3bc5bf5ac192b5edf6
DxdCode <davidmuela2407@gmail.com>
2025-08-27 16:55:34 -0500

    Ajustes en Reserva

--- backend/src/routers/ReservaRouter.js
+++ backend/src/routers/ReservaRouter.js
@@ -1,12 +1,14 @@
 
 import {Router} from 'express'
-import { actualizarReserva, crearReserva, eliminarReserva, visualizarReservas } from '../controllers/ReservasContoller.js';
+import { actualizarReserva, crearReserva, eliminarReserva, visualizarReservas, visualizarUnaReserva } from '../controllers/ReservasContoller.js';
 import { verificarTokenJWT } from '../middlewares/JWT.js';
 
 const router = Router()
 
 router.post("/reserva", verificarTokenJWT, crearReserva);
 router.get("/reserva", verificarTokenJWT, visualizarReservas);
+router.get("/reserva/:id", verificarTokenJWT, visualizarUnaReserva);
+
 router.put("/reserva/:id", verificarTokenJWT, actualizarReserva);
 router.delete("/reserva/:id", verificarTokenJWT, eliminarReserva);
 
